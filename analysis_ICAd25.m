fa9762ce1d4435b0acf41f022593b57fec261ac2
Peter Zhukovsky <38474927+peterzhukovsky@users.noreply.github.com>
2022-09-16 14:08:21 -0400

    Add files via upload

--- analysis_ICAd25.m
+++ analysis_ICAd25.m
@@ -18,8 +18,6 @@ rmat=reshape(Fnetmats_all(sub, :,:), [21 21]);D=21;
 full_icad25(sub,:)=rmat(triu(ones(D),1)==1)';
 rmat=reshape(Pnetmats_all(sub, :,:), [21 21]);D=21;
 part_icad25(sub,:)=rmat(triu(ones(D),1)==1)';
-rmat=reshape(Rmat_all(sub, :,:), [21 21]);D=21; rmat=r2z(rmat);
-rmat_icad25(sub,:)=rmat(triu(ones(D),1)==1)';
 end; clear rmat %% reshape the netmats to one line from the square form
 %%%%% didnt pass registration QC
 full_icad25(78,:)=NaN;full_icad25(93,:)=NaN;full_icad25(99,:)=NaN;full_icad25(253,:)=NaN;full_icad25(262,:)=NaN; full_icad25(429,:)=NaN; full_icad25(534,:)=NaN; full_icad25(569,:)=NaN; 
@@ -31,7 +29,7 @@ part_icad25(isnan(Fnetmats_all(:,1,1)),:)=[]; full_icad25(isnan(Fnetmats_all(:,1
 fmri_metrics(isnan(Fnetmats_all(:,1,1)),:)=[]; %% cut down the sheets to only include the non-NaN values, ie those with amyloid data
 
 %sum(~isnan(Fnetmats_all(:,1,1)))
-clinicalA.cdrcat=ones(length(subjects),1);clinicalA.cdrcat(clinicalA.cdrcat==1)=NaN; clinicalA.cdrcat(clinicalA.cdr==0)=0;clinicalA.cdrcat(clinicalA.cdr==0.5)=1;
+clinicalA.cdrcat=ones(length(subjects),1);clinicalA.cdrcat(clinicalA.cdrcat==1)=NaN; clinicalA.cdrcat(clinicalA.cdr==0)=0;clinicalA.cdrcat(clinicalA.cdr==0.5)=0.5;clinicalA.cdrcat(clinicalA.cdr==1)=1;
 clinicalA.risk=zeros(length(subjects),1); 
 
 tmp=table(amyloidA.tracer, amyloidA.PET_fSUVR_rsf_TOT_CORTMEAN);mdl=fitlm(tmp); 
@@ -44,12 +42,20 @@ for i=1:441; comb(i)=cellstr(strcat(combs{i,2}, combs{i,1})); end
 ICs=reshape(comb, [21,21]); clear comb combs i
 ICs_vector=ICs(triu(ones(D),1)==1)';
 
+mean_rms=dlmread('mean_rms_boldfiles.txt');
+subs_rms=readtable('mean_rms_boldfiles_ids.txt', 'ReadVariableNames', 0);subs_rms.Properties.VariableNames={'Subject'};
+subs_rms.mean_rms=mean_rms;
+clinicalA=innerjoin(subs_rms, clinicalA);
+
+
 %% Education analysis - step 2
 ix=amyloidA.PET_fSUVR_rsf_TOT_CORTMEAN~=0 & abs(clinicalA.delta_AmyloidA)<365 & clinicalA.dx2num==0 & mean_fd<0.1 & (clinicalA.apoe==34|clinicalA.apoe==44|clinicalA.apoe==33);%ix=logical(ones(675,1)); 
+ix=amyloidA.PET_fSUVR_rsf_TOT_CORTMEAN~=0 & abs(clinicalA.delta_AmyloidA)<365 & clinicalA.dx2num==0 & clinicalA.mean_rms <0.3;% & (clinicalA.apoe==34|clinicalA.apoe==44|clinicalA.apoe==33);%ix=logical(ones(675,1)); 
+
 permutations=1000;
 
 for i=1:length(part_icad25(1,:))
-        allobservations=clean(part_icad25(:,i),3); clin=clinicalA(~isnan(allobservations),:);p=prop_signal(~isnan(allobservations));m=mean_fd(~isnan(allobservations)); a=allobservations(~isnan(allobservations)); 
+        allobservations=clean(part_icad25(:,i),3); clin=clinicalA(~isnan(allobservations),:);p=prop_signal(~isnan(allobservations));m=clinicalA.mean_rms(~isnan(allobservations)); a=allobservations(~isnan(allobservations)); 
         i
         parfor n = 1:permutations; 
         permutation_index = randperm(length(a));
@@ -70,18 +76,18 @@ ICs_vector(p_perm_edu<0.05)
 square_P_part = zeros(D);
 square_P_part(triu(ones(D),1)>0) = p_perm_edu;
 square_T_part = zeros(D); square_T_part(triu(ones(D),1)>0) = Tedu; square_T_part(square_P_part>0.05)=0;
-square_T = square_T_full + square_T_part'; 
+square_T = square_T_part'; 
 figure;imagesc(square_T); set(gca,'XTick',[1:21], 'YTick',[1:21]); %clear square_* 
 
 ids=1:210;figure(131);
-for i=ids(p_perm_edu<0.01)
-    j=find(i==ids(p_perm_edu<0.01))
+for i=ids(p_perm_edu<0.05)
+    j=find(i==ids(p_perm_edu<0.05))
     allobservations=part_icad25(:,i);
-    clear T;T=table(clinicalA.Education, clinicalA.risk, clinicalA.cdrcat, clinicalA.Age, clinicalA.M_F,prop_signal,mean_fd, abs(clinicalA.delta_AmyloidA), clean(allobservations,3) ); 
+    clear T;T=table(clinicalA.Education, clinicalA.risk, clinicalA.cdrcat, clinicalA.Age, clinicalA.M_F,prop_signal,clinicalA.mean_rms, abs(clinicalA.delta_AmyloidA), clean(allobservations,3) ); 
     T.Properties.VariableNames={'edu', 'risk', 'cdrcat', 'Age', 'M_F','prop_signal', 'mean_fd', 'abs_delta_AmyloidA', 'FC'}; T=T(ix,:);
     mdl = fitlm(T,'FC~edu+risk+cdrcat+Age+M_F+prop_signal+mean_fd+abs_delta_AmyloidA+Age*M_F'); anovamdl=anova(mdl);
     mdl_p(j)=mdl.coefTest;
-    subplot(3,5,j); scatter(T.edu(T.cdrcat==0), mdl.Fitted(T.cdrcat==0), 5,'filled', 'b'); hold on; scatter(T.edu(T.cdrcat==1), mdl.Fitted(T.cdrcat==1), 5,'filled', 'r');
+    subplot(3,5,j); scatter(T.edu(T.cdrcat==0), mdl.Fitted(T.cdrcat==0), 5,'filled', 'b'); hold on; scatter(T.edu(T.cdrcat>=0.5), mdl.Fitted(T.cdrcat>=0.5), 5,'filled', 'r');
     m= fitlm(T.edu,mdl.Fitted);
     [ypred,yci] = predict(m,T.edu, 'Alpha',0.001); hold on
     yci=[yci,T.edu]; yci=sortrows(yci, 3);
@@ -108,19 +114,19 @@ cognitiveA.aLOGIMEM(cognitiveA.Animal==0)=NaN;
 clinicalA.delta_cog_t=abs(cognitiveA.Days_since_start-fmri_metrics.RunA_days);
 
 %ix=abs(clinicalA.delta_cog_t)<365;%ix=logical(ones(969,1)); 
-ix=amyloidA.PET_fSUVR_rsf_TOT_CORTMEAN~=0 & abs(clinicalA.delta_cog_t)<365 & clinicalA.dx2num==0 & (clinicalA.apoe==34|clinicalA.apoe==44|clinicalA.apoe==33);%ix=logical(ones(675,1)); 
+ix=amyloidA.PET_fSUVR_rsf_TOT_CORTMEAN~=0 & abs(clinicalA.delta_cog_t)<365 & clinicalA.dx2num==0 %& (clinicalA.apoe==34|clinicalA.apoe==44|clinicalA.apoe==33);%ix=logical(ones(675,1)); 
 
-  clear T;T=table(cognitiveA.tmt_cor, cognitiveA.BOSTON,cognitiveA.MEMUNITS, cognitiveA.aLOGIMEM, clinicalA.Education, clinicalA.risk, clinicalA.cdrcat, clinicalA.Age, clinicalA.M_F, abs(clinicalA.delta_cog_t),   abs(clinicalA.delta_AmyloidA) ); 
-  T.Properties.VariableNames={'TMT', 'BOSTON', 'MEMUNITS', 'LOGIMEM','education', 'risk', 'cdrcat', 'Age', 'M_F', 'abs_delta_cogA', 'abs_delta_AmyloidA'};T=T(ix,:);
+  clear T;T=table(clinicalA.apoe, cognitiveA.tmt_cor, cognitiveA.BOSTON,cognitiveA.MEMUNITS, cognitiveA.aLOGIMEM, clinicalA.Education, clinicalA.risk, clinicalA.cdrcat, clinicalA.Age, clinicalA.M_F, amyloidA.PET_fSUVR_rsf_TOT_CORTMEAN, abs(clinicalA.delta_cog_t),  abs(clinicalA.delta_AmyloidA) ); 
+  T.Properties.VariableNames={'apoe','TMT', 'BOSTON', 'MEMUNITS', 'LOGIMEM','education', 'risk', 'cdrcat', 'Age', 'M_F','amyloid', 'abs_delta_cogA', 'abs_delta_AmyloidA'};T=T(ix,:);
 
- mdl = fitlm(T,'LOGIMEM~education+risk+cdrcat+Age+M_F+abs_delta_AmyloidA+abs_delta_cogA+abs_delta_cogA+Age*M_F')
+ mdl = fitlm(T,'LOGIMEM~education+amyloid+cdrcat+Age+M_F+abs_delta_AmyloidA+abs_delta_cogA+abs_delta_cogA+Age*M_F')
   subplot(2,2,3);
   scatter(T.education(T.cdrcat==0), mdl.Fitted(T.cdrcat==0), 5,'filled', 'b'); hold on; scatter(T.education(T.cdrcat==1), mdl.Fitted(T.cdrcat==1), 5,'filled', 'r');
     m= fitlm(T.education,mdl.Fitted);
     [ypred,yci] = predict(m,T.education, 'Alpha',0.001); hold on
     yci=[yci,T.education]; yci=sortrows(yci, 3);    plot(T.education, ypred, 'k', 'LineWidth', 2);    plot(yci(:,3), yci(:,1), 'k', 'LineWidth', 0.5);    plot(yci(:,3), yci(:,2), 'k', 'LineWidth', 0.5);     r=round(corr(T.education, mdl.Fitted, 'rows', 'pairwise'),2);xlim([7 25]);     text(8, min(mdl.Fitted), strcat('R=',num2str(r)) ); %ylim([4 14]);
       
-  mdl = fitlm(T,'MEMUNITS~education+risk+cdrcat+Age+M_F+abs_delta_AmyloidA+abs_delta_cogA+Age*M_F')
+  mdl = fitlm(T,'MEMUNITS~education+amyloid+apoe+cdrcat+Age+M_F+abs_delta_AmyloidA+abs_delta_cogA+Age*M_F')
   subplot(2,2,4);
   scatter(T.education(T.cdrcat==0), mdl.Fitted(T.cdrcat==0), 5,'filled', 'b'); hold on; scatter(T.education(T.cdrcat==1), mdl.Fitted(T.cdrcat==1), 5,'filled', 'r');
     m= fitlm(T.education,mdl.Fitted);
@@ -177,8 +183,8 @@ ICs_vector=ICs(triu(ones(D),1)==1)';
 ic_mapping=readtable('ic_order_mapping.csv');
 tmp=ic_mapping.original_order_num;
 
-square_P_full = zeros(D); square_P_full(triu(ones(D),1)>0) = p_perm_inter(tmp); 
-square_T_full = zeros(D); square_T_full(triu(ones(D),1)>0) = Tinteract(tmp); square_T_full(square_P_full>0.05)=0;
+square_P_full = zeros(D); square_P_full(triu(ones(D),1)>0) = p_perm_edu(tmp); 
+square_T_full = zeros(D); square_T_full(triu(ones(D),1)>0) = Tedu(tmp); square_T_full(square_P_full>0.05)=0;
 figure(1);imagesc(square_T_full); set(gca,'XTick',[1:21], 'YTick',[1:21]); %clear square_* 
 
 square_P_full = zeros(D); square_P_full(triu(ones(D),1)>0) = p_perm_edu(tmp); 
@@ -198,3 +204,28 @@ hold on; myColorMap=zeros(21,3);myColorMap(:,3)=1; circularGraph(Weights_square,
 Weights=plsweights3; Weights(Weights<upperlim & Weights>lowerlim)=0;
 Weights(Weights<0)=0; Weights_square= zeros(21); Weights_square(triu(ones(21),1)>0) = abs(Weights);
 hold on; myColorMap=zeros(21,3);myColorMap(:,1)=1; circularGraph(Weights_square, 'Colormap',myColorMap, 'Label',myLabel);
+
+
+
+%% %
+for i=1:length(part_icad25(1,:))
+        allobservations=clean(full_icad25(:,i),3); clin=clinicalA(~isnan(allobservations),:);p=prop_signal(~isnan(allobservations));m=mean_fd(~isnan(allobservations)); a=allobservations(~isnan(allobservations)); 
+        i
+        parfor n = 1:permutations; 
+        permutation_index = randperm(length(a));
+        randomSample = a(permutation_index,:);
+        T=table(clin.Education, clin.risk, clin.cdrcat, clin.Age, clin.M_F,p,m, abs(clin.delta_AmyloidA), randomSample ); %clear clin p m a
+        T.Properties.VariableNames={'edu', 'risk', 'cdrcat', 'Age', 'M_F','prop_signal', 'mean_fd', 'abs_delta_AmyloidA', 'FC'};T=T(ix(~isnan(allobservations)),:);
+        mdl = fitlm(T,'FC~edu+risk+cdrcat+Age+M_F+prop_signal+mean_fd+abs_delta_AmyloidA+Age*M_F'); 
+        anovamdl=anova(mdl); F(1,n)=anovamdl.F(1);
+        end; 
+        F_permedu(i,:)=F;
+  clear T;T=table(clinicalA.Education, clinicalA.risk, clinicalA.cdrcat, clinicalA.Age, clinicalA.M_F,prop_signal,mean_fd, abs(clinicalA.delta_AmyloidA), clean(allobservations,3) ); 
+  T.Properties.VariableNames={'edu', 'risk', 'cdrcat', 'Age', 'M_F','prop_signal', 'mean_fd', 'abs_delta_AmyloidA', 'FC'};T=T(ix,:);
+  mdl = fitlm(T,'FC~edu+risk+cdrcat+Age+M_F+prop_signal+mean_fd+abs_delta_AmyloidA+Age*M_F'); anovamdl=anova(mdl);
+  p_perm_edu(i)=1-sum(anovamdl.F(1)>F_permedu(i,:))/permutations; 
+  Tedu(1,i)=mdl.Coefficients.tStat(2); Pedu(1,i)=mdl.Coefficients.pValue(2);
+end
+square_P_full = zeros(D); square_P_full(triu(ones(D),1)>0) = p_perm_edu; 
+square_T_full = zeros(D); square_T_full(triu(ones(D),1)>0) = Tedu; square_T_full(square_P_full>0.05)=0;
+ICs_vector(p_perm_edu<0.05)
--- pls_ICAd25_m.m
+++ pls_ICAd25_m.m
@@ -21,6 +21,11 @@ ICs_vector=ICs(triu(ones(D),1)==1)';
 
 ix=abs(clinical_final.delta_fmri)<365 & fmri_finalA.Motion_QC<=1 & clinical_final.dx2num_1neuro_2PD_3thyroid_4CVD_5medicineCogDys_6B12def_7Seizur==0;%ix=logical(ones(675,1)); 
 
+mean_rms=dlmread('mean_rms_boldfiles_rep.txt');
+subs_rms=readtable('mean_rms_boldfiles_ids_rep.txt', 'ReadVariableNames', 0);subs_rms.Properties.VariableNames={'Subject'};
+subs_rms.mean_rms=mean_rms;
+clinical_final=innerjoin(subs_rms, clinical_final);
+
 %% setting the missing values to NaN instead of 0
 cognitive_final.TRAILB(cognitive_final.Animal==0)=NaN;
 cognitive_final.TRAILBRR(cognitive_final.Animal==0)=NaN;
@@ -33,26 +38,28 @@ ids=cellfun(@isempty,  strfind(ICs_vector, 'IC4')) & cellfun(@isempty,  strfind(
 %ispresent = cellfun(@(s) ~isempty(strfind(string, s)), elements)
 
 Y=[cognitive_final.tmt_cor,cognitive_final.WAIS ];%   clinical_final.Age
-Y=[cognitive_final.MEMUNITS,clinical_final.Age];%  %(isnan(Y)')' ,cognitive_final.aLOGIMEM,, 
+Y=[cognitive_final.MEMUNITS];%  %(isnan(Y)')' ,cognitive_final.aLOGIMEM,, ,clinical_final.Age
 X=part_icad25A;%(:,ids==1); %X(96,:)=NaN;
 
 %%% remove nans - 
-naninx=sum(isnan(X)')'>0| sum(isnan(Y)')' >0  |clinical_final.cdr>1.5 |~ix; Y=Y(naninx==0,:);  X=X(naninx==0,:); clin=clinical_final(naninx==0,:); mean_fd=mean_fd(naninx==0,:); prop_signal=prop_signal(naninx==0,:); %|clinical_final.Age<50 |mean_fd>0.1
+%naninx=sum(isnan(X)')'>0| sum(isnan(Y)')' >0  |clinical_final.cdr>1 |~ix; Y=Y(naninx==0,:);  X=X(naninx==0,:); clin=clinical_final(naninx==0,:); mean_fd=mean_fd(naninx==0,:); prop_signal=prop_signal(naninx==0,:); %|clinical_final.Age<50 |mean_fd>0.1
+naninx=sum(isnan(X)')'>0| isnan(Y) >0  |clinical_final.cdr>1 |~ix | clinical_final.mean_rms>0.3; Y=Y(naninx==0,:);  X=X(naninx==0,:); clin=clinical_final(naninx==0,:); amy=amyloid_final(naninx==0,:);mean_fd=mean_fd(naninx==0,:); prop_signal=prop_signal(naninx==0,:); %|clinical_final.Age<50 |mean_fd>0.1
 clinical_pls=clinical_final(naninx==0,:); 
 Y=zscore(Y); X=zscore(X);x=X;
-ncomp=4
+ncomp=3
 %max(partialcorr(X,Y, [clin.Age,prop_signal,mean_fd, clin.delta_fmri]))
 
 %%% regress out sex and age and potentially other covariates
 %mdl = fitlm(table(clin.Age,  clin.M_F, clin.delta_fmri, Y(:,1)) ); Y(:,1)=mdl.Residuals.Raw;
-%mdl = fitlm(table(clin.Age,  clin.M_F, clin.delta_fmri, Y(:,2)) ); Y(:,2)=mdl.Residuals.Raw; %clin.Age, 
-%clear x; for i=1:length(X(1,:)); mdl = fitlm(table(clin.M_F,mean_fd, X(:,i)) ); x(:,i)=mdl.Residuals.Raw;end
+mdl = fitlm(table(clin.Age, clin.M_F, clin.delta_fmri, Y(:,1)) ); Y(:,1)=mdl.Residuals.Raw;
+clear x; for i=1:length(X(1,:)); mdl = fitlm(table(clin.M_F,clin.Age, clin.mean_rms, X(:,i)) ); x(:,i)=mdl.Residuals.Raw;end
 
 [XL,YL,XS,YS,BETA,PCTVAR,MSE,stats] = plsregress(x,Y,ncomp);PCTVAR
 %%% explore the PLS components and their correlation with Y
 %figure;imagesc(corr(XS,Y)); 
- XS=XS*-1; c=corr(XS, Y) %1.TMT  2.Tower  3.PAL  4.DSST
-combs=allcomb([1:3], [1:2]);figure(13);
+%XS=XS*-1; 
+c=corr(XS, Y) %1.TMT  2.Tower  3.PAL  4.DSST
+combs=allcomb([1:3], [1:1]);figure(13);
 for i=1:length(combs)
 hold off; ix1=combs(i,1);ix2=combs(i,2); subplot(3,2,i); 
 scatter(XS(clin.cdr==0,ix1), Y(clin.cdr==0,ix2), 5,'b', 'filled'); 
@@ -85,14 +92,15 @@ for ncomp=1:4
     p_1(ncomp)=sum(sum(PCTVAR(1,:))<Rsq1')/permutations
 end
 [XL,YL,XS,YS,BETA,PCTVAR,MSE,stats] = plsregress(x,Y,ncomp);PCTVAR
+figure; histogram(Rsq'); sum(PCTVAR')
 
 %% bootstrapping to get the func connectivity weights for PLS1, 2 and 3
 dim=4
 [XL,YL,XS,YS,BETA,PCTVAR,MSE,stats] = plsregress(x,Y,dim);PCTVAR
-PLS3w=stats.W(:,3);
+PLS1w=stats.W(:,1);PLS2w=stats.W(:,2);PLS3w=stats.W(:,3);
 
 bootnum=5000;
-PLS3weights=[];
+PLS1weights=[];PLS2weights=[];PLS3weights=[];
 
 parfor i=1:bootnum
     i;
@@ -101,32 +109,52 @@ parfor i=1:bootnum
     Xr=x(myresample,:); % define X for resampled subjects
     Yr=Y(myresample,:); % define X for resampled subjects
     [XL,YL,XS,YS,BETA,PCTVAR,MSE,stats]=plsregress(Xr,Yr,dim); %perform PLS for resampled data
-      
-    newW=stats.W(:,3);%extract PLS2 weights
+    
+    newW=stats.W(:,1);%extract PLS1 weights
+    if corr(PLS1w,newW)<0 % the sign of PLS components is arbitrary - make sure this aligns between runs
+        newW=-1*newW;
+    end
+    PLS1weights=[PLS1weights,newW]; %store (ordered) weights from this bootstrap run    
+    
+    newW=stats.W(:,2);%extract PLS2 weights
+    if corr(PLS2w,newW)<0 % the sign of PLS components is arbitrary - make sure this aligns between runs
+        newW=-1*newW;
+    end
+    PLS2weights=[PLS2weights,newW]; %store (ordered) weights from this bootstrap run    
+  
+    newW=stats.W(:,3);%extract PLS3 weights
     if corr(PLS3w,newW)<0 % the sign of PLS components is arbitrary - make sure this aligns between runs
         newW=-1*newW;
     end
     PLS3weights=[PLS3weights,newW]; %store (ordered) weights from this bootstrap run    
 end
 
+PLS1sw=std(PLS1weights');
+plsweights1=PLS1w./PLS1sw';
+PLS2sw=std(PLS2weights');
+plsweights2=PLS2w./PLS2sw';
 PLS3sw=std(PLS3weights');
 plsweights3=PLS3w./PLS3sw';
 
 %plsweights=zeros(1,210); plsweights(ids)=plsweights3;
 %plsweights3=plsweights;
-ICs_vector(plsweights3>3)
-ICs_vector(plsweights3<-3)
-c
+ICs_vector(plsweights1>3)
+ICs_vector(plsweights1<-3)
+
+ICs_vector(plsweights2>3)
+ICs_vector(plsweights2<-3)
+
 %doublecheck:%ICs_vector_cut=ICs_vector(ids);ICs_vector_cut(plsweights3>3)ICs_vector_cut(plsweights3<-3)
 
-
+[coef,scores,~,~,explained] = pca(x);
+corr(coef(:,1), plsweights1)
 %% visuals for individual brain connectivities
 rvalues=corr(X, Y(:,1), 'rows', 'pairwise')
 figure; scatter(rvalues, plsweights3)
 
 ids=1:210;figure(2);hold off
-for i=ids(plsweights3>3)
-    j=find(i==ids(plsweights3>3))
+for i=ids(plsweights1>3)
+    j=find(i==ids(plsweights1>3))
     allobservations=full_icad25(:,i);
     subplot(3,3,j); scatter(X(clin.cdr==0,i), Y(clin.cdr==0,1), 3,'filled', 'k'); hold on; scatter(X(clin.cdr==0.5,i), Y(clin.cdr==0.5,1), 3,'filled', 'k');
     m=fitlm(X(:,i),Y(:,1));
@@ -141,44 +169,48 @@ for i=ids(plsweights3>3)
     %text(-4, min(Y(:,1)), strcat('R=',num2str(r)) ); %ylim([-1 1]);
     xlabel(ICs_vector{i}); clear r; 
 end
-amy=[1.09400000000000;0.899000000000000;0.970000000000000;0.853000000000000;1.08100000000000;1.00400000000000;1.10500000000000;0.943000000000000;1.34000000000000;3.47800000000000;1.32500000000000;1.01200000000000;3.15200000000000;1.06000000000000;1.02100000000000;1.08500000000000;2.87800000000000;1.14800000000000;1.10300000000000;1.22400000000000;1.07300000000000;1.22000000000000;0.750000000000000;0.998000000000000;0.946000000000000;1.04000000000000;1.33100000000000;3.98400000000000;0.808000000000000;2.03000000000000;3.24000000000000;1.16100000000000;0.977000000000000;0.918000000000000;1.40900000000000;1.01000000000000;2.56000000000000;1.00500000000000;0.846000000000000;1.06000000000000;1.02100000000000;1.18600000000000;2.46900000000000;0.881000000000000;0.942000000000000;2.50900000000000;0.986000000000000;4.18300000000000;1.08100000000000;1.07500000000000;0.989000000000000;1.58100000000000;2.03300000000000;1.06400000000000;1.76500000000000;0.882000000000000;1.27900000000000;1.01400000000000;1.86800000000000;1.01300000000000;2.23500000000000;1.21800000000000;0.545000000000000;0.749000000000000;1.01300000000000;2.41600000000000;0.805000000000000;1.14500000000000;1.85900000000000;2.14200000000000;3.24500000000000;3.54200000000000;2.86800000000000;2.24800000000000;3.39200000000000;0.987000000000000;1.00700000000000;0.963000000000000;1.06600000000000;1.10500000000000;0.948000000000000;1.11100000000000;1.78100000000000;1.07500000000000;1.43700000000000;1.02600000000000;2.47600000000000;1.07100000000000;1.99100000000000;0.827000000000000;3.71200000000000;1.16900000000000;1.02000000000000;1.14200000000000;3.33600000000000;1.09700000000000;1.65800000000000;1.29700000000000;0.936000000000000;0.897000000000000;0.958000000000000;1.06000000000000;1.03700000000000;3.72300000000000;2.19800000000000;1.24800000000000;2.96200000000000;1.19400000000000;2.03200000000000;1.07400000000000;2.95200000000000;1.24200000000000;1.80100000000000;0.744000000000000;2.15000000000000;0.960000000000000;0.594000000000000;0.737000000000000;1.05500000000000;2.29700000000000;0.987000000000000;2.23300000000000;1.91600000000000;1.84500000000000;1.49400000000000;2.58000000000000;1.03700000000000;1.56200000000000;0.975000000000000;1.06000000000000;1.12500000000000;3.81900000000000;0.824000000000000;1.34900000000000;2.87700000000000;0.760000000000000;1.21600000000000;2.36600000000000;0.985000000000000;0.738000000000000;1.67900000000000;0.987000000000000;2.34300000000000;1.57300000000000;2.24400000000000;2.53100000000000;2.04200000000000;1.06000000000000;2.19100000000000;1.39000000000000;1.04900000000000;2.51900000000000;4.21400000000000;2.38500000000000;2.54500000000000;1.06700000000000;0.977000000000000;1.02700000000000;0.998000000000000;0.889000000000000;2.59900000000000;5.41800000000000;1.06300000000000;2.25800000000000;1.22100000000000;0.966000000000000;2.40000000000000;2.04200000000000;1.02100000000000;1.81900000000000;2.27000000000000;0.827000000000000;0.848000000000000;1.84700000000000;1.70300000000000;1.02400000000000;0.985000000000000;0.659000000000000;1.72300000000000;2.77800000000000;1.01800000000000;3.27700000000000;1.20700000000000;2.19600000000000;1.96100000000000;2.89700000000000;2.98100000000000;1.01100000000000;2.61600000000000;1.03600000000000;3.25100000000000;1.22300000000000;1.03400000000000;1.05500000000000;1.03600000000000;2.45800000000000;2.49200000000000;1.62600000000000;2.73000000000000;0.999000000000000;1.04600000000000;1.31600000000000;1.11000000000000;0.961000000000000;1.39500000000000;3.80400000000000;1.10600000000000;1.03400000000000;3.61500000000000;3.47900000000000;1.65500000000000;0.989000000000000;0.991000000000000;1.09100000000000;0.994000000000000;1.05900000000000;0.939000000000000;1.90900000000000;1.99000000000000;1.20800000000000;2.81300000000000;0.999000000000000;1.38300000000000;3.10300000000000;1.02700000000000;0.966000000000000;2.68800000000000;1.00900000000000;0.980000000000000;2.48200000000000;0.855000000000000;1.91400000000000;0.995000000000000;1.06300000000000;0.722000000000000;3.04400000000000;1.96000000000000;1.70600000000000;1.04000000000000;1.03100000000000;0.893000000000000;1.01100000000000;2.39700000000000;0.856000000000000;2.32200000000000;0.923000000000000;0.994000000000000;2.34800000000000;1.12100000000000;0.949000000000000;0.768000000000000;0.947000000000000;1.13600000000000;0.857000000000000;2.48300000000000;0.882000000000000;1.17000000000000;2.87600000000000;3.96700000000000;1.08900000000000;2.04100000000000;1.28400000000000;0.924000000000000;1.10600000000000;0.755000000000000;2.32700000000000;0.963000000000000;2.01500000000000;2.38900000000000;1.08100000000000;0.920000000000000;0.984000000000000;2.10800000000000;0.949000000000000;0.996000000000000;0.795000000000000;0.891000000000000;3.41200000000000;2.02100000000000;1.26800000000000;1.27100000000000;1.09800000000000;1.01000000000000;0.983000000000000;1.95000000000000;1.07500000000000;3.07600000000000;0.736000000000000;0.933000000000000;0.946000000000000;1.00700000000000;1.43500000000000;1.12500000000000;2.27700000000000;1.01400000000000;1.55700000000000;1.05000000000000;1.15300000000000;2.14000000000000;2.30600000000000;3.05200000000000;1.06200000000000;2.55600000000000;0.899000000000000;1.34200000000000;0.960000000000000;1.01200000000000;2.37000000000000;1.55200000000000;1.63700000000000;2.73500000000000;1.28900000000000;1.55500000000000;2.48800000000000;2.16000000000000;1.11800000000000;0.973000000000000;0.987000000000000;0.901000000000000;1.53000000000000;1.93500000000000;0.886000000000000;0.805000000000000;0.818000000000000;2.13600000000000;1.07600000000000;2.00700000000000;0.905000000000000;2.75900000000000;1.24200000000000;0.949000000000000;0.994000000000000;1.06000000000000;1.15300000000000;1.38300000000000;1.03300000000000;2.29700000000000;1.00900000000000;2.92500000000000;1.58500000000000;2.63700000000000;3.00800000000000;1.89300000000000;1.68500000000000;1.09200000000000;3.41800000000000;1.03200000000000;3.41700000000000;2.09200000000000;1.04600000000000;1.03800000000000;0.952000000000000;0.862000000000000;2.41200000000000;2.00300000000000;0.921000000000000;0.801000000000000;1.10700000000000;2.53200000000000;0.992000000000000;0.949000000000000;1.08400000000000;3.75900000000000;0.926000000000000;1.02800000000000;1.44000000000000;1.04400000000000;0.933000000000000;0.870000000000000;1.21400000000000;1.40900000000000;0.853000000000000;1.22100000000000;4.01500000000000;1.05400000000000;1.39600000000000;1.14900000000000;1.02700000000000;0.900000000000000;0.808000000000000;2.33600000000000;3.49800000000000;1.92700000000000;0.651000000000000;0.927000000000000;1.41700000000000;1.12800000000000;1.04300000000000;1.00800000000000;1.03200000000000;2.74900000000000;0.923000000000000];
-figure(3);hold off
-for i=ids(plsweights3<-4)
-    j=find(i==ids(plsweights3<-4))
+
+ids=1:210;figure;hold off
+for i=ids(plsweights1>3)
+    j=find(i==ids(plsweights1>3))
     allobservations=part_icad25(:,i);
-    subplot(3,3,j); scatter(amy, X(:,i), 3,'filled', 'k'); hold on; 
-    m=fitlm(amy, X(:,i));
-    [ypred,yci] = predict(m,amy, 'Alpha',0.001); hold on
-    yci=[yci,amy]; yci=sortrows(yci, 3);
-    plot(amy, ypred, 'k', 'LineWidth', 2);
+    subplot(3,3,j); scatter(amy.PET_fSUVR_rsf_TOT_CORTMEAN, X(:,i), 3,'filled', 'k'); hold on; 
+    m=fitlm(amy.PET_fSUVR_rsf_TOT_CORTMEAN, X(:,i));
+    [ypred,yci] = predict(m,amy.PET_fSUVR_rsf_TOT_CORTMEAN, 'Alpha',0.001); hold on
+    yci=[yci,amy.PET_fSUVR_rsf_TOT_CORTMEAN]; yci=sortrows(yci, 3);
+    plot(amy.PET_fSUVR_rsf_TOT_CORTMEAN, ypred, 'k', 'LineWidth', 2);
     plot(yci(:,3), yci(:,1), 'k', 'LineWidth', 0.5);
     plot(yci(:,3), yci(:,2), 'k', 'LineWidth', 0.5); 
-    r=round(corr(X(:,i), amy, 'rows', 'pairwise'),2)%xlim([7 25]); 
-    %text(0, min(Y(:,1)), strcat('R=',num2str(r)) ); %ylim([-1 1]);
+    r=round(corr(X(:,i), amy.PET_fSUVR_rsf_TOT_CORTMEAN, 'rows', 'pairwise'),2)%xlim([7 25]); 
+    text(0, min(Y(:,1)), strcat('R=',num2str(r)) ); %ylim([-1 1]);
+     xlabel(ICs_vector{i}); clear r; 
 end
 
+%% visualize the weights using circularGraph(x);
 %% visualize the weights using circularGraph(x);
 component=1; 
 ica2yeo7=readtable('D:\Canada_2020\UK_biobank\reports\ica2yeo7.csv');
 nodes=dlmread('D:\Canada_2020\UK_biobank\reports\icad25_nodesordered.csv'); tmp=nanmean(X(clin.cdr==0,:))';
-%nodes=[plsweights3,(1:210)',tmp, nodes]; nodes=sortrows(nodes, component);
+nodes=[plsweights3,(1:210)',tmp, nodes]; nodes=sortrows(nodes, component);
 
-myLabel=ica2yeo7.Yeo7N;figure
+ic_mapping=readtable('D:\Canada_2020\OASIS\reports\fmri\ic_order_mapping.csv');
+tmp=ic_mapping.original_order_num;
+
+myLabel=ica2yeo7.Yeo7N;myLabel=myLabel([1 7 9 13 14 20 21 5 6 16 10 11 12 17 3 2 4 8 19 15 18]);
+myLabel=repmat({''}, [21,1]); figure
 upperlim=3; lowerlim=-3; %upperlim=nodes(205,component); lowerlim=nodes(5,component);
-Weights=plsweights3; Weights(Weights<upperlim & Weights>lowerlim)=0;%Weights=stats.W(:,component); 
+Weights=plsweights1(tmp); Weights(Weights<upperlim & Weights>lowerlim)=0;%Weights=stats.W(:,component); 
 Weights(Weights>0)=0; Weights_square= zeros(21); Weights_square(triu(ones(21),1)>0) = abs(Weights);
 hold on; myColorMap=zeros(21,3);myColorMap(:,3)=1; circularGraph(Weights_square, 'Colormap',myColorMap, 'Label',myLabel);
-Weights=plsweights3; Weights(Weights<upperlim & Weights>lowerlim)=0;
+Weights=plsweights1(tmp); Weights(Weights<upperlim & Weights>lowerlim)=0;
 Weights(Weights<0)=0; Weights_square= zeros(21); Weights_square(triu(ones(21),1)>0) = abs(Weights);
 hold on; myColorMap=zeros(21,3);myColorMap(:,1)=1; circularGraph(Weights_square, 'Colormap',myColorMap, 'Label',myLabel);
 
-%%% average connectivity visual
-figure; 
-Weights=tmp;
-Weights(Weights>-0.04)=0; Weights_square= zeros(21); Weights_square(triu(ones(21),1)>0) = abs(Weights);
+myLabel=ica2yeo7.Yeo7N;figure
+upperlim=3; lowerlim=-3; %upperlim=nodes(205,component); lowerlim=nodes(5,component);
+Weights=plsweights1; Weights(Weights<upperlim & Weights>lowerlim)=0;%Weights=stats.W(:,component); 
+Weights(Weights>0)=0; Weights_square= zeros(21); Weights_square(triu(ones(21),1)>0) = abs(Weights);
 hold on; myColorMap=zeros(21,3);myColorMap(:,3)=1; circularGraph(Weights_square, 'Colormap',myColorMap, 'Label',myLabel);
-
-Weights=tmp;
-Weights(Weights<0.03)=0; Weights_square= zeros(21); Weights_square(triu(ones(21),1)>0) = abs(Weights);
+Weights=plsweights1; Weights(Weights<upperlim & Weights>lowerlim)=0;
+Weights(Weights<0)=0; Weights_square= zeros(21); Weights_square(triu(ones(21),1)>0) = abs(Weights);
 hold on; myColorMap=zeros(21,3);myColorMap(:,1)=1; circularGraph(Weights_square, 'Colormap',myColorMap, 'Label',myLabel);
-
